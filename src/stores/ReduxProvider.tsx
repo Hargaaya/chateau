a9272b45fbb6526b0cb892bf2dcd97c39066c0dd
Hargaaya Idris <hargaaya10@gmail.com>
2023-06-23 00:22:58 +0200

    implement redux persistance

--- src/stores/ReduxProvider.tsx
+++ src/stores/ReduxProvider.tsx
@@ -2,6 +2,8 @@
 
 import { Provider } from "react-redux";
 import { createStore } from "@/stores/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
 type Props = {
   children: React.ReactNode;
@@ -10,6 +12,13 @@ type Props = {
 
 export function ReduxProvider({ children, preloadedState }: Props) {
   const store = createStore(preloadedState);
+  const persistor = persistStore(store);
 
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
--- src/stores/store.tsx
+++ src/stores/store.tsx
@@ -1,15 +1,46 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
 import { reducer as settingsReducer } from "./slices/settingsSlice";
+import createWebStorage from "redux-persist/lib/storage/createWebStorage";
 
-export function createStore(preloadedState = {}) {
+const createNoopStorage = () => {
+  return {
+    getItem(_key: any) {
+      return Promise.resolve(null);
+    },
+    setItem(_key: any, value: any) {
+      return Promise.resolve(value);
+    },
+    removeItem(_key: any) {
+      return Promise.resolve();
+    },
+  };
+};
+
+const storage = typeof window !== "undefined" ? createWebStorage("local") : createNoopStorage();
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedSettingsReducer = persistReducer(persistConfig, settingsReducer);
+
+export const createStore = (preloadedState = {}) => {
   const store = configureStore({
     reducer: {
-      settings: settingsReducer,
+      settings: persistedSettingsReducer,
     },
     preloadedState,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }),
   });
 
   return store;
-}
+};
 
 export const store = createStore({});
