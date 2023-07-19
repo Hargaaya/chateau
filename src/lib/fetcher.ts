6a5ba18ad5119bdfafe73d65c4c47b16939f9316
Hargaaya <hargaaya10@gmail.com>
2023-07-20 00:52:15 +0200

    add fetcher function for swr

---
+++ src/lib/fetcher.ts
@@ -0,0 +1,7 @@
+async function fetcher<JSON = any>(input: RequestInfo, init?: RequestInit): Promise<JSON> {
+  const res = await fetch(input, init);
+
+  return res.json();
+}
+
+export default fetcher;
