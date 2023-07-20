3fb3452838f0278f39ae7664a8f3970abbe7485e
Hargaaya <hargaaya10@gmail.com>
2023-07-20 02:26:35 +0200

    cleanup 🧼

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import Chat from "@/components/Chat";
+import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
 import fetcher from "@/lib/fetcher";
+import { RefreshCcw } from "lucide-react";
 import useSWR from "swr";
 
 type Props = {
@@ -13,13 +15,27 @@ type Props = {
 
 export default function Page({ params }: Props) {
   const { id } = params;
-  const { data, error } = useSWR<Chat>(`/api/chat/${id}`, fetcher);
+  const { data, error, mutate } = useSWR<Chat>(`/api/chat/${id}`, fetcher);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      {error && <p>An error occured getting your history. Try the button below in a while 🐢</p>}
+      {error && (
+        <div className="mx-auto max-w-3xl py-6 flex flex-col gap-4 text-center">
+          <p>An error occured getting your history. Try the button below in a while 🐢</p>
+          <Button variant="ghost" className="hover:bg-yellow-500" onClick={() => mutate()}>
+            <RefreshCcw className="mr-2" size="16px" />
+            Refresh
+          </Button>
+        </div>
+      )}
       {!data && (
-        <Skeleton className="max-w-3xl w-full h-[90vh] m-10 rounded-lg bg-secondary flex items-center justify-center">Loading...</Skeleton>
+        <div className="mx-auto w-full max-w-3xl py-6 flex flex-col gap-4">
+          <Skeleton className="h-48 rounded-lg bg-secondary" />
+          <Skeleton className="h-32 rounded-lg bg-secondary" />
+          <Skeleton className="h-24 rounded-lg bg-secondary" />
+          <Skeleton className="h-56 rounded-lg bg-secondary" />
+          <Skeleton className="h-32 rounded-lg bg-secondary" />
+        </div>
       )}
       {data && <Chat id={id} initialMessages={data.messages} />}
     </main>
