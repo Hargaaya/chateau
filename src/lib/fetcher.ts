90dc10983573236175af9809841756b9c93b2054
Hargaaya <hargaaya10@gmail.com>
2023-07-23 03:52:50 +0200

    update fetcher to throw errors

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,5 +1,18 @@
+export interface ErrorInfo extends Error {
+  status: number;
+  info: any;
+}
+
 async function fetcher<JSON = any>(input: RequestInfo, init?: RequestInit): Promise<JSON> {
   const res = await fetch(input, init);
+
+  if (!res.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    (error as ErrorInfo).status = res.status;
+    (error as ErrorInfo).info = await res.json();
+
+    throw error;
+  }
 
   return res.json();
 }
