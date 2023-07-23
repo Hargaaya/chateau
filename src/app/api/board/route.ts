8d9ad1b5b371b0c5376826d57802be37215b876e
Hargaaya <hargaaya10@gmail.com>
2023-07-23 06:41:55 +0200

    refactor

--- src/app/api/board/route.ts
+++ src/app/api/board/route.ts
@@ -8,31 +8,31 @@ async function getBoards() {
   if (!user?.email) return NextResponse.redirect("/");
 
   let boardRepository = new BoardRepository(user.email);
-  let data = boardRepository.getBoards();
+  let data = await boardRepository.getBoards();
 
-  return NextResponse.json({ data });
+  return NextResponse.json(data);
 }
 
 async function createBoard(req: NextRequest) {
   const { user } = (await getServerSession(authOptions)) as Session;
   if (!user?.email) return NextResponse.redirect("/");
 
-  const board = await req.json();
+  const { board } = await req.json();
   let boardRepository = new BoardRepository(user.email);
   const data = await boardRepository.createBoard(board);
 
-  return NextResponse.json({ data });
+  return NextResponse.json(data);
 }
 
 async function updateBoard(req: NextRequest) {
   const { user } = (await getServerSession(authOptions)) as Session;
   if (!user?.email) return NextResponse.redirect("/");
-  const board = await req.json();
 
+  const { board } = await req.json();
   let boardRepository = new BoardRepository(user.email);
-  const data = await boardRepository.updateBoard(board);
+  await boardRepository.updateBoard(board);
 
-  return NextResponse.json({ data });
+  return NextResponse.json(null, { status: 200 });
 }
 
 async function deleteBoard(req: NextRequest) {
