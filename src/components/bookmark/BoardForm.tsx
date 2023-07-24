94444e3e212896aeb1e07e1f63c27efffcea2197
Hargaaya <hargaaya10@gmail.com>
2023-07-25 00:51:13 +0200

    improve board listing

--- src/components/bookmark/BoardForm.tsx
+++ src/components/bookmark/BoardForm.tsx
@@ -1,6 +1,6 @@
 import fetcher from "@/lib/fetcher";
-import { Select, SelectTrigger, SelectValue, SelectContent, SelectGroup, SelectItem } from "@radix-ui/react-select";
-import React, { useState } from "react";
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectGroup, SelectItem } from "@/components/ui/select";
+import React, { useEffect, useState } from "react";
 import useSWR from "swr";
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
@@ -10,11 +10,27 @@ type Props = {
 };
 
 const BoardForm = ({ snippet }: Props) => {
-  const { data: boards, error } = useSWR<Board[]>("/api/board", fetcher);
-  const [selectedBoard, setSelectedBoard] = useState<string>("");
+  const { data: boards, error, mutate } = useSWR<Board[]>("/api/board", fetcher);
+  const [selectedBoard, setSelectedBoard] = useState<string | null>(null);
+  const [valueExists, setValueExists] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!selectedBoard) return;
+
+    const board = boards?.find((board) => board._id === selectedBoard);
+    if (!board) return;
+
+    const snippetExists = board.snippets.find((s) => s.hash === snippet.hash);
+    if (snippetExists) {
+      setValueExists(true);
+      return;
+    }
+
+    setValueExists(false);
+  }, [selectedBoard, boards, snippet.hash]);
 
   const handleSaveClick = () => {
-    if (selectedBoard === "") return;
+    if (!selectedBoard) return;
     const board = boards?.find((board) => board._id === selectedBoard);
     if (!board) return;
 
@@ -29,14 +45,24 @@ const BoardForm = ({ snippet }: Props) => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ board }),
+    }).then((res) => {
+      if (res.ok) mutate();
     });
+  };
+
+  const checkExistence = (board: Board, snippetHash: string): string => {
+    const isInList = board.snippets.find((s) => s.hash === snippetHash);
+    if (!isInList) {
+      if (board.snippets.length === 0) return "Empty";
+      return board.snippets.length.toString();
+    }
+
+    return "Already saved";
   };
 
   if (error) return <p>Failed to load boards</p>;
 
   if (!boards) return <Skeleton className="col-span-3 h-10 bg-secondary" />;
-
-  console.log(boards);
 
   return (
     <React.Fragment>
@@ -51,13 +77,13 @@ const BoardForm = ({ snippet }: Props) => {
                 {Array.isArray(boards) &&
                   boards.map((board: Board) => (
                     <SelectItem key={board._id} value={board._id as string}>
-                      {board.name}
+                      {board.name} ({checkExistence(board, snippet.hash)})
                     </SelectItem>
                   ))}
               </SelectGroup>
             </SelectContent>
           </Select>
-          <Button className="col-span-1" onClick={handleSaveClick}>
+          <Button className="col-span-1" onClick={handleSaveClick} disabled={valueExists}>
             Save
           </Button>
         </React.Fragment>
