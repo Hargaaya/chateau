820981835ff054f451be8dc07ef332e8ae59891d
Hargaaya <hargaaya10@gmail.com>
2023-07-27 07:33:06 +0200

    add forking functionality

--- src/components/chat-messages/ChatContent.tsx
+++ src/components/chat-messages/ChatContent.tsx
@@ -4,17 +4,41 @@ import Highlighter from "./Highlighter";
 import { Message } from "ai";
 import ChatAvatar from "./ChatAvatar";
 import isLastInArray from "@/utils/isLastInArray";
+import { nanoid } from "nanoid";
+import { mutate } from "swr";
 
 type Props = {
   messages: Message[];
+  title: string;
 };
 
-const ChatContent = ({ messages }: Props) => {
+const ChatContent = ({ messages, title }: Props) => {
   const lastChatRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     lastChatRef.current?.scrollIntoView({ behavior: "smooth" });
   }, [messages]);
+
+  async function forkChat(index: number) {
+    const _id = nanoid();
+    const scopedMessages = messages.slice(0, index + 1);
+
+    await fetch("/api/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        _id,
+        title: `fork of: ${title}`,
+        messages: scopedMessages,
+      }),
+    }).then((res) => {
+      if (res.ok) {
+        mutate("/api/chat/history");
+      }
+    });
+  }
 
   return (
     <React.Fragment>
@@ -26,7 +50,7 @@ const ChatContent = ({ messages }: Props) => {
           <React.Fragment key={index}>
             <div ref={isLast ? lastChatRef : null} className="flex gap-4 w-full py-4">
               <ChatAvatar role={role} />
-              <Highlighter message={content} />
+              <Highlighter index={index} message={content} forkChat={forkChat} />
             </div>
             {!isLast && <Separator className="w-11/12 mx-auto" orientation="horizontal" />}
           </React.Fragment>
--- src/components/chat-messages/Highlighter.tsx
+++ src/components/chat-messages/Highlighter.tsx
@@ -1,25 +1,48 @@
-import React from "react";
+import React, { MouseEvent } from "react";
 import CodeBlock from "./CodeBlock";
 import codeFormatter from "@/utils/codeFormatter";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { Button } from "../ui/button";
+import { Copy, GitForkIcon } from "lucide-react";
+import copyToClipboard from "@/utils/copyToClipboard";
 
 type Props = {
+  index: number;
   message: string;
+  forkChat: (index: number) => void;
 };
 
-const Highlighter = ({ message }: Props) => {
+const Highlighter = ({ index, message, forkChat }: Props) => {
   const splitMessage = message.split("```");
+  const forkChatHandler = (_: MouseEvent<HTMLButtonElement>) => forkChat(index);
 
   return (
-    <div className="font-light pt-2 whitespace-pre-wrap">
-      {splitMessage.map((text, index) => {
-        if (index % 2 === 0) {
-          return <p key={index}>{text}</p>;
-        } else {
-          const { language, formattedText: code } = codeFormatter(text);
-          return <CodeBlock key={index} language={language} code={code} />;
-        }
-      })}
-    </div>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <div className="font-light pt-2 whitespace-pre-wrap w-full">
+            {splitMessage.map((text, index) => {
+              if (index % 2 === 0) {
+                return <p key={index}>{text}</p>;
+              } else {
+                const { language, formattedText: code } = codeFormatter(text);
+                return <CodeBlock key={index} language={language} code={code} />;
+              }
+            })}
+          </div>
+        </TooltipTrigger>
+        <TooltipContent className="flex">
+          <Button variant="ghost" className="flex gap-2" onClick={forkChatHandler}>
+            <GitForkIcon size="16px" />
+            Fork
+          </Button>
+          <Button variant="ghost" className="flex gap-2" onClick={() => copyToClipboard(message)}>
+            <Copy size="16px" />
+            Copy
+          </Button>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 };
 
